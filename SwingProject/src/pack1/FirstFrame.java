3b553f45bb4ab7296524d6e356982045d40b1438
Özgür İnce <ozgur@ince.ist>
2021-10-11 15:15:19 +0300

    init

---
+++ SwingProject/src/pack1/FirstFrame.java
@@ -0,0 +1,63 @@
+package pack1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FirstFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtName;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FirstFrame frame = new FirstFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FirstFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(48, 11, 86, 20);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String name = txtName.getText();
+				JOptionPane.showMessageDialog(null, name);
+			}
+		});
+		btnSave.setBounds(48, 45, 89, 23);
+		contentPane.add(btnSave);
+	}
+}
